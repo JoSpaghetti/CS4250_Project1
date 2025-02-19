d8eb4a6bb9e2af9108865c98e80717e20aea86cf
JoSpaghetti <143671979+JoSpaghetti@users.noreply.github.com>
2025-02-19 10:08:54 -0800

    Web Scraper: language detection, added doc strings, files download into HTML

--- Code/CodePart1-WebCrawler/web_scraper.py
+++ Code/CodePart1-WebCrawler/web_scraper.py
@@ -2,12 +2,16 @@ import sys
 import urllib.request as urllib
 from urllib.error import URLError, HTTPError
 from urllib.parse import urlparse
+import langid
 
 
 def read_webpage(url):
-    """Given a URL, it returns the string value of a webpage"""
-    #read HTML from URL
-    try:
+    """Given a URL, it returns the string value of a webpage\n
+    Arguments: url (string): the URL to fetch\n
+    Returns: the string value of the webpage
+    """
+
+    try: #read HTML from URL
         webpage_text = urllib.urlopen(url)
         webpage_bytes = webpage_text.read()
     except HTTPError as e:
@@ -19,10 +23,15 @@ def read_webpage(url):
         return webpage_string
 
 
-def download_to_file(text, filename):
-    """Given text and a filename, it writes it to a file"""
+def download_to_file(text, filename, extension="html"):
+    """Given text and a filename, it writes it to a file\n
+    Arguments: text (string): the text to write\n
+    Arguments: filename (string): the name of the file\n
+    Arguments: extension (string): the extension of the file [the default is "html"]\n
+    Returns: None
+    """
     #format filename to repository directory
-    filename = f"Repository/{filename}.txt"
+    filename = f"Repository/{filename}.{extension}"
 
     #throw error if text or filename is null or empty
     if text is None or filename is None:
@@ -42,25 +51,34 @@ def download_to_file(text, filename):
         file.close()
 
 def url_format(url):
-    """Given a URL, it returns the validity of said URL"""
+    """Given a URL, it returns the validity of said URL \n
+    Arguments: url (string): a URL to validate \n
+    Returns: boolean expression based on validity of URL"""
     try:
         valid_url = urlparse(url)
         return all([valid_url.scheme, valid_url.netloc])
     except AttributeError:
         return False
 
+def detect_language(text):
+    """Using the langid library, it returns the language of a string sequence \n
+    Arguments: text (string): the text to identify\n
+    Returns: language of given input
+    """
+    return langid.classify(text)[0]
 
 def main():
     url = "https://www.google.com"
-    if url_format(url) == False :
+
+    if not url_format(url):
         # work on code to handle malformed url strings
         print("Bad URL")
         sys.exit()
 
     web_string = read_webpage(url)
-    print(web_string)
+    web_string_language = detect_language(web_string)
+    print(web_string, web_string_language)
     download_to_file(web_string, "web_scrape_demo")
-
 
 if __name__=="__main__":
     main()
